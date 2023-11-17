533cb519782eabb8de32b0810c8a8fb5d1ee2885
孙圣科 <sunshengke@inspur.com>
2023-11-17 16:04:07 +0800

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 # streamlit_app.py
-
 import streamlit as st
 
 # Initialize connection.
 conn = st.connection('mysql', type='sql')
 
 # Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+df = conn.query('SELECT * from t1;', ttl=0)
 
 # Print results.
 for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(f"{row.name} has a :{row.age}:")
